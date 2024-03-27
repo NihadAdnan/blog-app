03dcd803abd9399a284720d43a29c95f9001a5f3
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-03-27 18:41:23 +0600

    contact page design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import styles from './contact.module.css'
+import Image from 'next/image'
+import contactImg from '/public/giphy2.gif'
 
 export default function ContactPage() {
   return (
-    <div>Contact Page</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>  
+        <Image src={contactImg} alt='contact us' fill className={styles.img}/>
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder='Name'/>
+          <input type="email" placeholder='Email'/>
+          <input type="number" placeholder='Phone Number (Optional)' />
+          <textarea cols="30" rows="10" placeholder='Message'></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
